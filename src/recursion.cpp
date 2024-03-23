11ff37aaeba619234bb09f6c0a36833c27671e67
Lisa <llvvss@icloud.com>
2024-03-23 13:15:48 -0600

    solve with recursion instead

--- src/recursion.cpp
+++ src/recursion.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int recursive_sum(int m, int n) {
+    // base case
+    if (m == n)
+        return m;
+    return m + recursive_sum(m + 1, n);
+}
+
 // sum numbers between m and n
 
 int main()
 {
-    // solve this problem using loops
-    int m=2, n=4;
-    int sum = 0;
-    for(int i=m; i <= n; i++) {
-        sum += i;
-    }
+    int m=1, n=5;
 
-    cout << "Sum= " << sum << endl;
+    // solve this problem using recursion
+    cout << "Sum = " << recursive_sum(m, n) << endl;
+
+    // // solve this problem using loops
+    // int sum = 0;
+    // for(int i=m; i <= n; i++) {
+    //     sum += i;
+    // }
+
+    // cout << "Sum= " << sum << endl;
 
     return 0;
 }
